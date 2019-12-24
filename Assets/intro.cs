58830fd6a10c88e83199420b8c534af688d42667
turrino <isidor.x@gmail.com>
2019-12-24 20:41:08 +0100

    working intro!

--- Assets/intro.cs
+++ Assets/intro.cs
@@ -1,23 +1,39 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Resources;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class intro : MonoBehaviour
 {
-
+    public intro2script Intro2;
+    public intro4script skipTo4;
     public Text SleepText;
     public SpriteRenderer clock1;
     public SpriteRenderer clock2;
     public SpriteRenderer hand;
     public SpriteRenderer ScimattaSleep;
     public GameObject Text1;
+    bool shakeclock;
+    float speed = 50f; //how fast it shakes
+    float amount = 0.03f; //how much it shakes
+    bool debugskip = false;
+
 
     void Start()
-    {
-        InvokeRepeating("Zzz", 0, 0.5f);
-        Invoke("AlarmOff", 3);
-        Invoke("Alarm", 5);
+    {        
+        if (debugskip)
+        {
+            skipTo4.Begin();
+        }
+        else
+        {
+            ScimattaSleep.enabled = true;
+            InvokeRepeating("Zzz", 0, 0.5f);
+            Invoke("AlarmOff", 2);
+            Invoke("Alarm", 4);
+        }
     }
 
     void Zzz()
@@ -37,6 +53,7 @@ public class intro : MonoBehaviour
     {
         clock1.enabled = false;
         clock2.enabled = true;
+        shakeclock = true;
         SleepText.enabled = false;
         Invoke("Hand", 1);
     }
@@ -44,6 +61,11 @@ public class intro : MonoBehaviour
     void Hand()
     {
         hand.enabled = true;
+    }
+
+    public void Skip()
+    {
+        SceneManager.LoadScene(NamesList.MainScenario, LoadSceneMode.Single);
     }
 
     private void Update()
@@ -57,9 +79,12 @@ public class intro : MonoBehaviour
                 hand.enabled = false;
                 ScimattaSleep.enabled = false;
                 Text1.SetActive(true);
+                Intro2.Begin();
             }
         }
+        if (shakeclock)
+        {
+            clock2.transform.position = clock2.transform.position + new Vector3(0, Mathf.Sin(Time.time * speed) * amount, 0);
+        }
     }
-
-
 }
